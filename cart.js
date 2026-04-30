855428fc893c9ace4393a99b725dc35e15683a22
RPM <fardeencp7@gmail.com>
2026-05-01 03:44:04 +0530

    Delete cart.js

--- cart.js
+++
@@ -1,7 +0,0 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-function addToCart(product){
-    cart.push(product);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    alert("Added to cart");
-}
